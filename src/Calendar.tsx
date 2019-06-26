64f6447c4ae20f7952c94130c4a0e262ac2d43ac
Bartlomiej Zuber <bartlomiej.zuber@outlook.com>
2019-06-26 17:57:35 +0200

    minor refactoring

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import moment, { Moment } from "moment";
 import "./Calendar.scss";
-import DaySpanColumn from "./components/DaySpanColumn/DaySpanColumn";
 import { getCurrentWeekDays } from "./utils/date-utils";
 import Timeline from "./components/Timeline/Timeline";
+import { DaySpan } from "./components/TimeSpans";
 import WeekHeader from "./components/WeekHeader/WeekHeader";
 
 interface CalendarProps {
@@ -18,15 +18,13 @@ const Calendar: React.FC<CalendarProps> = ({ locale }) => {
     setDaysOfWeek(getCurrentWeekDays());
   }, [baseMoment, locale]);
 
-  console.log(daysOfWeek);
-
   return (
     <div className="calendar">
       <WeekHeader daysOfWeek={daysOfWeek} />
       <div className="calendar_body">
         <Timeline />
         {daysOfWeek.map(day => (
-          <DaySpanColumn key={day.dayOfYear()} day={day} />
+          <DaySpan key={day.dayOfYear()} day={day} />
         ))}
       </div>
     </div>
