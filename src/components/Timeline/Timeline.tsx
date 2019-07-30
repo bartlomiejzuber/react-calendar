d18288f8e70a99099ce73fe914edb1fead942897
Bartlomiej Zuber <bartlomiej.zuber@outlook.com>
2019-07-30 11:34:00 +0200

    timeline component updated

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./Timeline.scss";
 import HourSpan from "../TimeSpans/HourSpan/HourSpan";
-import { HOURS_IN_DAY } from "../../const/timeConst";
+import { HOURS_IN_DAY_ARRAY } from "../../const/timeConst";
 
-interface TimelineProps {
-  
-}
+interface TimelineProps {}
 
-const hoursArray = Array.from(Array(HOURS_IN_DAY), (x, index) => index + 1);
 const Timeline: React.FC<TimelineProps> = () => {
   return (
     <div className="timeline">
-      {hoursArray.map(hour => (
-        <HourSpan key={hour} className="time-line_hour-span" hour={hour} withoutQuarters/>
+      {HOURS_IN_DAY_ARRAY.map(hour => (
+        <HourSpan
+          key={hour}
+          className="time-line_hour-span"
+          hour={hour}
+          withoutQuarters
+        />
       ))}
     </div>
   );
